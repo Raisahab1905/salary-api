92a92aee1127d17e3b31c5c06e5be275b6d017f8
rai <raiparteek19azm@gmail.com>
2025-09-22 11:53:40 +0530

    files commited

--- terraform/main.tf
+++ terraform/main.tf
@@ -223,6 +223,13 @@ resource "aws_security_group" "scylla_sg" {
     security_groups = [aws_security_group.app_sg.id]  # allow app to connect
   }
 
+  ingress {
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    security_groups = [aws_security_group.bastion_sg.id]  # allow app to connect
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
